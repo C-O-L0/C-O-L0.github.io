58f02f194ec8ebae17b43a2df326448b40fd6ef8
C-O-L0 <72121233+C-O-L0@users.noreply.github.com>
2025-03-23 15:16:51 +0800

    Delete Web Dev For Beginners directory

--- Web Dev For Beginners/Week 7/Bank-App/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import RegisterForm from "./components/RegisterForm";
-import LoginForm from "./components/LoginForm";
-
-function App() {
-  return <RegisterForm></RegisterForm>;
-}
-
-export default App;
